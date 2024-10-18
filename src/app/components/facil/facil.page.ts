882ee460fbcbbdbac15003241ae5028098dcbf80
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-18 00:37:14 -0300

    juego con logica

--- src/app/components/facil/facil.page.ts
+++ src/app/components/facil/facil.page.ts
@@ -17,6 +17,7 @@ import {
 } from '@ionic/angular/standalone';
 import { UtilService } from 'src/app/services/util.service';
 import { Imagen } from 'src/app/models/imagen';
+import { Alert } from 'src/app/models/alert';
 
 @Component({
   selector: 'app-facil',
@@ -50,7 +51,11 @@ export class FacilPage implements OnInit {
   colores: string[] = ['primary', 'tertiary', 'success'];
   imagenes: Imagen[] = [];
   detenerTimer: boolean = false;
+  carta1: Imagen | null = null;
+  carta2: Imagen | null = null;
   tiempo: string = '00:00';
+  segundos: number = 0;
+  contadorBuenas: number = 0;
 
   constructor() {
     for (let index = 0; index < this.path.length; index++) {
@@ -67,13 +72,12 @@ export class FacilPage implements OnInit {
   }
   timer() {
     // Ejecuta la función cada 1 segundo (1000 milisegundos)
-    let segundos = 0;
     const timer = setInterval(() => {
-      segundos++;
+      this.segundos++;
 
       // Calcular minutos y segundos
-      const minutos = Math.floor(segundos / 60);
-      const segs = segundos % 60;
+      const minutos = Math.floor(this.segundos / 60);
+      const segs = this.segundos % 60;
 
       // Formatear con ceros a la izquierda si es necesario
       const minutosFormateados = minutos < 10 ? '0' + minutos : minutos;
@@ -90,6 +94,47 @@ export class FacilPage implements OnInit {
   }
 
   darVueltaCard(index: number) {
-    this.imagenes[index].mostrar = true;
+    const carta = this.imagenes[index];
+    if (this.carta1 && this.carta2) {
+      Alert.error('No podes dar vuelta la carta ', 'tonto');
+    } else if (!this.carta1) {
+      carta.mostrar = true;
+      this.carta1 = carta;
+    } else if (!this.carta2) {
+      carta.mostrar = true;
+      this.carta2 = carta;
+      setTimeout(() => {
+        this.verificar();
+      }, 500);
+    }
+  }
+
+  verificar() {
+    if (this.carta1 && this.carta2 && this.carta1.path === this.carta2.path) {
+      console.log('coinciden');
+      this.contadorBuenas++;
+      if (this.contadorBuenas === this.path.length) {
+        this.detenerTimer = true;
+        console.log('Ganaste');
+        //Pregunto si queres seguir jugando
+        this.reiniciarJuego();
+      }
+    } else if (this.carta1 && this.carta2) {
+      Alert.error('No coinciden', '');
+      this.carta1.mostrar = false;
+      this.carta2.mostrar = false;
+    }
+    this.carta1 = null;
+    this.carta2 = null;
+  }
+
+  reiniciarJuego() {
+    this.util.desordenarArray(this.imagenes);
+    this.imagenes.forEach((item) => {
+      item.mostrar = false;
+    });
+    this.contadorBuenas = 0;
+    this.segundos = 0;
+    this.detenerTimer = false;
   }
 }
